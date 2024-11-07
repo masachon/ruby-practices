f3d65e38ec77cead30ca6e948a67c8d964199a1b
masachon <masashimiyazawa58@gmail.com>
2024-11-07 14:07:53 +0900

    lsコマンドを作る２

--- 04.ls/ls_like.rb
+++ 04.ls/ls_like.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-def list_files
+def list_files(show_all)
   # 現在のディレクトリのファイルを取得し、ソート
-  Dir.glob('*').sort
+  Dir.glob('*', show_all ? File::FNM_DOTMATCH : 0).sort
 end
 
 # 列数を定数として設定
@@ -43,5 +43,6 @@ def format_and_display(files)
 end
 
 # メインプログラム
-files = list_files
+show_all = ARGV.include?('-a') # -aオプションが含まれているかを確認
+files = list_files(show_all)
 format_and_display(files)
